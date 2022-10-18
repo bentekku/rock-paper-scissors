68d143d36c79924312f97af7facd9cc6475be798
Shadab Khan <mdshadab21july@gmail.com>
2022-10-18 15:23:20 +0530

    Add comments to js file

--- script.js
+++ script.js
@@ -1,75 +1,85 @@
-function playRound (playerSelection, computerSelection)
-{
-    if(playerSelection === "")
-        {
-            alert("Player decision required to calculate the outcome.");
-        }
-    else {
-        for (i = 0; i <= 5; i++)
-        {
-            if(playerSelection === computerSelection)
-            {
-                console.log("It's a tie!");
-            }
-                    // Win-scenarios
-            else if (playerSelection === "rock" && computerSelection === "scissor")
-            {
-                console.log(`You Win! ${playerSelection.toUpperCase()} beats ${computerSelection.toUpperCase()}` );
-
-                playerPoints++;
-            }
-            else if(playerSelection === "paper" && computerSelection === "rock")
-            {
-                console.log(`You Win! ${playerSelection.toUpperCase()} beats ${computerSelection.toUpperCase()}` );
-
-                playerPoints++;
-            }
-            else if(playerSelection === "scissor" && computerSelection === "paper")
-            {
-                console.log(`You Win! ${playerSelection.toUpperCase()} beats ${computerSelection.toUpperCase()}`);
-
-                playerPoints++;
-            }
-
-                    // Lose-scenarios
-            else if(playerSelection === "paper" && computerSelection === "scissor")
-            {
-                console.log(`You Lose! ${computerSelection.toUpperCase()} beats ${playerSelection.toUpperCase()}` );
-
-                computerPoints++;
-            }
-            else if(playerSelection === "rock" && computerSelection === "paper")
-            {
-                console.log(`You Lose! ${computerSelection.toUpperCase()} beats ${playerSelection.toUpperCase()}`);
-
-                computerPoints++;
-            }
-            else if(playerSelection === "scissor" && computerSelection === "rock")
-            {
-                console.log(`You Lose! ${computerSelection.toUpperCase()} beats ${playerSelection.toUpperCase()}`);
-
-                computerPoints++;
-            }
-            else if(playerSelection != "rock" || playerSelection != "paper" || playerSelection != "scissor"){
-                console.log("Please select one of the options: ROCK, PAPER OR SCISSOR");
-            }
-
-            playerSelection = prompt("Rock, Paper or Scissor?: ").toLowerCase();
-            computerSelection = getComputerChoice().toLowerCase();
-        }
-    }
-    // console.log(`Round one is over. \n Player Points: ${playerPoints} \n Computer Points: ${computerPoints}`);
-}
-
-function getComputerChoice()
-{
-    return choices[Math.floor(Math.random() * choices.length)];
-}
-
+// VARIABLES
 let playerPoints = 0;
 let computerPoints = 0;
 
 const choices = ["rock", "paper", "scissor"];
+
+//  Main function which is responsible for the comparision of player and computer selections, resulting in text which declares who's
+//  the winner of the game
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === "") {
+    alert("Player decision required to calculate the outcome.");
+  } else {
+    // Giving five chances to the player and counting best of five
+    for (i = 0; i <= 5; i++) {
+      if (playerSelection === computerSelection) {
+        console.log("It's a tie!");
+      }
+
+      // Win-scenarios
+      else if (playerSelection === "rock" && computerSelection === "scissor") {
+        console.log(
+          `You Win! ${playerSelection.toUpperCase()} beats ${computerSelection.toUpperCase()}`
+        );
+
+        playerPoints++;
+      } else if (playerSelection === "paper" && computerSelection === "rock") {
+        console.log(
+          `You Win! ${playerSelection.toUpperCase()} beats ${computerSelection.toUpperCase()}`
+        );
+
+        playerPoints++;
+      } else if (
+        playerSelection === "scissor" &&
+        computerSelection === "paper"
+      ) {
+        console.log(
+          `You Win! ${playerSelection.toUpperCase()} beats ${computerSelection.toUpperCase()}`
+        );
+
+        playerPoints++;
+      }
+
+      // Lose-scenarios
+      else if (playerSelection === "paper" && computerSelection === "scissor") {
+        console.log(
+          `You Lose! ${computerSelection.toUpperCase()} beats ${playerSelection.toUpperCase()}`
+        );
+
+        computerPoints++;
+      } else if (playerSelection === "rock" && computerSelection === "paper") {
+        console.log(
+          `You Lose! ${computerSelection.toUpperCase()} beats ${playerSelection.toUpperCase()}`
+        );
+
+        computerPoints++;
+      } else if (
+        playerSelection === "scissor" &&
+        computerSelection === "rock"
+      ) {
+        console.log(
+          `You Lose! ${computerSelection.toUpperCase()} beats ${playerSelection.toUpperCase()}`
+        );
+
+        computerPoints++;
+      } else if (
+        playerSelection != "rock" ||
+        playerSelection != "paper" ||
+        playerSelection != "scissor"
+      ) {
+        console.log("Please select one of the options: ROCK, PAPER OR SCISSOR");
+      }
+
+      playerSelection = prompt("Rock, Paper or Scissor?: ").toLowerCase();
+      computerSelection = getComputerChoice().toLowerCase();
+    }
+  }
+  // console.log(`Round one is over. \n Player Points: ${playerPoints} \n Computer Points: ${computerPoints}`);
+}
+
+function getComputerChoice() {
+  return choices[Math.floor(Math.random() * choices.length)];
+}
 
 const playerSelection = prompt("Rock, Paper or Scissor?: ").toLowerCase();
 const computerSelection = getComputerChoice().toLowerCase();
